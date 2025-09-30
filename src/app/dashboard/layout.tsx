dc55af5af4eb690bacc32715877d7b38636a39e8
farrel irawan <irawanssfarrel@gmail.com>
2025-09-30 15:25:19 +0700

    feat: enhance DashboardPageLayout with AppSidebar and improved layout structure

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,6 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
+import AppSidebar from "@/components/AppSidebar/app-sidebar";
 
 export default function DashboardPageLayout({
   children,
@@ -7,8 +9,12 @@ export default function DashboardPageLayout({
 }>) {
   return (
     <SidebarProvider>
+      <AppSidebar />
       <section className="w-full h-screen">
-        <SidebarTrigger />
+        <div className="w-full flex items-center gap-2 h-12 overflow-hidden px-3">
+          <SidebarTrigger />
+          <Separator orientation="vertical" className="max-h-6" />
+        </div>
         {children}
       </section>
     </SidebarProvider>
