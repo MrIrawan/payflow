ffa7ded4fcda250dc5c6b4f5039f85b6999df642
farrel irawan <irawanssfarrel@gmail.com>
2025-09-30 21:21:44 +0700

    feat: add CollapsibleMenuProps interface with children and items structure

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,4 +1,16 @@
+import { LucideIcon } from "lucide-react";
+
 export interface BreadcrumbsProps {
     countItem: number;
     href: Array<string> | string;
+}
+
+export interface CollapsibleMenuProps {
+    children: React.ReactNode;
+    items: [
+        {
+            title: string;
+            icon: LucideIcon;
+        }
+    ];
 }
