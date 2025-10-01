3ccd972f968099d8f93f50702fd4295d899b6a4b
farrel irawan <irawanssfarrel@gmail.com>
2025-10-02 00:42:27 +0700

    feat: replace individual cards with QuickNavigationCard for streamlined navigation

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,6 @@
-import { Button } from "@/components/ui/button";
+import QuickNavigationCard from "@/components/QuickNavigationCard/quick-navigation-card";
+import { quickNavigationData } from "../../../public/data/static-quick-navigation";
+
 import {
   Card,
   CardHeader,
@@ -7,12 +9,6 @@ import {
   CardContent,
   CardFooter,
 } from "@/components/ui/card";
-import {
-  UserRoundIcon,
-  CalendarCheck2Icon,
-  HandCoinsIcon,
-  AlarmClockIcon,
-} from "lucide-react";
 
 export default function DashboardPage() {
   return (
@@ -33,50 +29,7 @@ export default function DashboardPage() {
           </CardHeader>
           <CardContent className="px-0 flex flex-col gap-3">
             <div className="flex items-center gap-3">
-              <Card className="w-84 h-20 shadow-none py-0 p-3 flex-row gap-3 cursor-pointer border-none ring ring-border transition-all duration-200 hover:ring-2 hover:ring-blue-300">
-                <div className="w-20 h-full bg-blue-200 ring-2 ring-blue-300 rounded-md flex items-center justify-center">
-                    <UserRoundIcon className="w-6 h-6 text-blue-500" />
-                </div>
-                <div className="w-full h-full flex flex-col gap-0.5">
-                  <h2 className="text-sm font-semibold">Data diri anda</h2>
-                  <p className="text-xs font-medium text-black/50">
-                    kelola data diri anda dengan mudah di PayFLow.
-                  </p>
-                </div>
-              </Card>
-              <Card className="w-84 h-20 shadow-none py-0 p-3 flex-row gap-3 cursor-pointer border-none ring ring-border transition-all duration-200 hover:ring-2 hover:ring-green-300">
-                <div className="w-20 h-full bg-green-200 ring-2 ring-green-300 rounded-md flex items-center justify-center">
-                    <CalendarCheck2Icon className="w-6 h-6 text-green-500" />
-                </div>
-                <div className="w-full h-full flex flex-col gap-0.5">
-                  <h2 className="text-sm font-semibold">Absensi anda</h2>
-                  <p className="text-xs font-medium text-black/50">
-                    dapatkan satistik absensi anda di waktu kapanpun
-                  </p>
-                </div>
-              </Card>
-              <Card className="w-84 h-20 shadow-none py-0 p-3 flex-row gap-3 cursor-pointer border-none ring ring-border transition-all duration-200 hover:ring-2 hover:ring-yellow-300">
-                <div className="w-20 h-full bg-yellow-200 ring-2 ring-yellow-300 rounded-md flex items-center justify-center">
-                    <HandCoinsIcon className="w-6 h-6 text-yellow-500" />
-                </div>
-                <div className="w-full h-full flex flex-col gap-0.5">
-                  <h2 className="text-sm font-semibold">Info gaji anda</h2>
-                  <p className="text-xs font-medium text-black/50">
-                    lihat informasi gaji anda kapanpun dimanapun
-                  </p>
-                </div>
-              </Card>
-              <Card className="w-84 h-20 shadow-none py-0 p-3 flex-row gap-3 cursor-pointer border-none ring ring-border transition-all duration-200 hover:ring-2 hover:ring-orange-300">
-                <div className="w-20 h-full bg-orange-200 ring-2 ring-orange-300 rounded-md flex items-center justify-center">
-                    <AlarmClockIcon className="w-6 h-6 text-orange-500" />
-                </div>
-                <div className="w-full h-full flex flex-col gap-0.5">
-                  <h2 className="text-sm font-semibold">Jam ajar & mapel</h2>
-                  <p className="text-xs font-medium text-black/50">
-                    atur mapel dan jam ajar sesuai yang anda mau
-                  </p>
-                </div>
-              </Card>
+              <QuickNavigationCard data={quickNavigationData}/>
             </div>
           </CardContent>
         </Card>
