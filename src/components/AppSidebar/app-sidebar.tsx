768e67a9a4a1e30e3eb243bfddde205a47d42c6d
farrel irawan <irawanssfarrel@gmail.com>
2025-10-01 08:52:00 +0700

    feat: replace hardcoded collapsible items with dynamic rendering from staticSidebarData

--- src/components/AppSidebar/app-sidebar.tsx
+++ src/components/AppSidebar/app-sidebar.tsx
@@ -1,3 +1,6 @@
+import CollapsibleMenu from "../CollapsibleMenu/collapsible-menu";
+import { staticSidebarData } from "../../../public/data/static-sidebar-menu";
+
 import {
   Sidebar,
   SidebarHeader,
@@ -7,10 +10,7 @@ import {
   SidebarGroupContent,
   SidebarMenu,
   SidebarMenuButton,
-  SidebarMenuSub,
   SidebarMenuItem,
-  SidebarMenuSubItem,
-  SidebarMenuSubButton,
 } from "../ui/sidebar";
 import {
   Collapsible,
@@ -20,14 +20,6 @@ import {
 import { Card } from "../ui/card";
 
 import {
-  UserRoundIcon,
-  UsersRoundIcon,
-  ChevronDownIcon,
-  CalendarArrowUpIcon,
-  CalendarDaysIcon,
-  CalendarCheckIcon,
-  CalendarArrowUp,
-  ClockIcon,
   HandCoinsIcon,
   BookIcon,
 } from "lucide-react";
@@ -43,133 +35,16 @@ export default function AppSidebar() {
         <SidebarGroup>
           <SidebarGroupContent>
             <SidebarMenu className="flex flex-col gap-3">
-              <Collapsible className="group/collapsible">
-                <SidebarMenuItem>
-                  <CollapsibleTrigger asChild>
-                    <SidebarMenuButton className="flex items-center justify-between">
-                      <div className="flex items-center gap-2">
-                        <UserRoundIcon className="w-5 h-5" />
-                        <p className="text-base font-zalando-medium">
-                          Data guru
-                        </p>
-                      </div>
-                      <ChevronDownIcon className="w-5 h-5" />
-                    </SidebarMenuButton>
-                  </CollapsibleTrigger>
-                  <CollapsibleContent>
-                    <SidebarMenuSub className="flex flex-col gap-2">
-                      <SidebarMenuSubItem>
-                        <SidebarMenuSubButton>
-                          <div className="flex items-center gap-2">
-                            <UsersRoundIcon className="w-4 h-4" />
-                            <p className="text-base font-zalando-medium">
-                              Data semua guru
-                            </p>
-                          </div>
-                        </SidebarMenuSubButton>
-                      </SidebarMenuSubItem>
-                      <SidebarMenuSubItem>
-                        <SidebarMenuSubButton>
-                          <div className="flex items-center gap-2">
-                            <UserRoundIcon className="w-4 h-4" />
-                            <p className="text-base font-zalando-medium">
-                              Data per-guru
-                            </p>
-                          </div>
-                        </SidebarMenuSubButton>
-                      </SidebarMenuSubItem>
-                    </SidebarMenuSub>
-                  </CollapsibleContent>
-                </SidebarMenuItem>
-              </Collapsible>
-              <Collapsible className="group/collapsible">
-                <SidebarMenuItem>
-                  <CollapsibleTrigger asChild>
-                    <SidebarMenuButton className="flex items-center justify-between">
-                      <div className="flex items-center gap-2">
-                        <CalendarArrowUpIcon className="w-5 h-5" />
-                        <p className="text-base font-zalando-medium">
-                          Absensi kehadiran
-                        </p>
-                      </div>
-                      <ChevronDownIcon className="w-5 h-5" />
-                    </SidebarMenuButton>
-                  </CollapsibleTrigger>
-                  <CollapsibleContent>
-                    <SidebarMenuSub className="flex flex-col gap-2">
-                      <SidebarMenuSubItem>
-                        <SidebarMenuSubButton>
-                          <div className="flex items-center gap-2">
-                            <CalendarArrowUp className="w-4 h-4" />
-                            <p className="text-base font-zalando-medium">
-                              Rata-rata absensi
-                            </p>
-                          </div>
-                        </SidebarMenuSubButton>
-                      </SidebarMenuSubItem>
-                      <SidebarMenuSubItem>
-                        <SidebarMenuSubButton>
-                          <div className="flex items-center gap-2">
-                            <CalendarDaysIcon className="w-4 h-4" />
-                            <p className="text-base font-zalando-medium">
-                              Absensi perbulan
-                            </p>
-                          </div>
-                        </SidebarMenuSubButton>
-                      </SidebarMenuSubItem>
-                      <SidebarMenuSubItem>
-                        <SidebarMenuSubButton>
-                          <div className="flex items-center gap-2">
-                            <CalendarCheckIcon className="w-4 h-4" />
-                            <p className="text-base font-zalando-medium">
-                              Absensi per-guru
-                            </p>
-                          </div>
-                        </SidebarMenuSubButton>
-                      </SidebarMenuSubItem>
-                    </SidebarMenuSub>
-                  </CollapsibleContent>
-                </SidebarMenuItem>
-              </Collapsible>
-              <Collapsible className="group/collapsible">
-                <SidebarMenuItem>
-                  <CollapsibleTrigger asChild>
-                    <SidebarMenuButton className="flex items-center justify-between">
-                      <div className="flex items-center gap-2">
-                        <ClockIcon className="w-5 h-5" />
-                        <p className="text-base font-zalando-medium">
-                          Jam mengajar
-                        </p>
-                      </div>
-                      <ChevronDownIcon className="w-5 h-5" />
-                    </SidebarMenuButton>
-                  </CollapsibleTrigger>
-                  <CollapsibleContent>
-                    <SidebarMenuSub className="flex flex-col gap-2">
-                      <SidebarMenuSubItem>
-                        <SidebarMenuSubButton>
-                          <div className="flex items-center gap-2">
-                            <BookIcon className="w-4 h-4" />
-                            <p className="text-base font-zalando-medium">
-                              Jam ajar per-mapel
-                            </p>
-                          </div>
-                        </SidebarMenuSubButton>
-                      </SidebarMenuSubItem>
-                      <SidebarMenuSubItem>
-                        <SidebarMenuSubButton>
-                          <div className="flex items-center gap-2">
-                            <UsersRoundIcon className="w-4 h-4" />
-                            <p className="text-base font-zalando-medium">
-                              Jam ajar per-guru
-                            </p>
-                          </div>
-                        </SidebarMenuSubButton>
-                      </SidebarMenuSubItem>
-                    </SidebarMenuSub>
-                  </CollapsibleContent>
-                </SidebarMenuItem>
-              </Collapsible>
+              {staticSidebarData.map((item, index) => (
+                <CollapsibleMenu key={index} items={item.items}>
+                    <div className="flex items-center gap-2">
+                      <item.labelIcon className="w-5 h-5" />
+                      <p className="text-base font-zalando-medium">
+                        {item.label}
+                      </p>
+                    </div>
+                </CollapsibleMenu>
+              ))}
               <SidebarMenuItem>
                 <SidebarMenuButton>
                   <div className="flex items-center gap-2">
