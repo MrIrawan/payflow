f479bdda1d1fe47d33d54676329b35b96fa30d8e
farrel irawan <irawanssfarrel@gmail.com>
2025-11-22 00:19:16 +0700

    feat: integrate sign up form data to sign up API

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -2,7 +2,7 @@ export interface SignUpRequest {
   first_name: string;
   last_name?: string;
   date_of_birth: Date;
-  gender: string | "male" | "female";
+  gender: "male" | "female";
   email_address: string;
   password_email: string;
 }
