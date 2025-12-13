847c5d80e632006d2f389691499da3b6e9326fea
farrel irawan <irawanssfarrel@gmail.com>
2025-12-13 20:07:29 +0700

    feat: implement new data card component on admin page with dummy data

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,4 +1,12 @@
-import { Card } from "@/components/ui/card";
+import {
+  DataCard,
+  DataCardBody,
+  DataCardFooter,
+  DataCardHeader,
+} from "@/components/DataCard/data-card";
+import { Button } from "@/components/ui/button";
+import { Card, CardDescription, CardTitle } from "@/components/ui/card";
+import { ArrowRightIcon, MarsIcon, Users2Icon, VenusIcon } from "lucide-react";
 
 export default function AdminPage() {
   return (
@@ -6,9 +14,78 @@ export default function AdminPage() {
       <div className="w-full flex flex-col gap-6">
         {/* Data card component */}
         <div className="flex flex-row justify-between items-center gap-6">
-          <Card className="w-full min-h-[200px]"></Card>
-          <Card className="w-full min-h-[200px]"></Card>
-          <Card className="w-full min-h-[200px]"></Card>
+          <DataCard className="w-full h-[250px] flex flex-col gap-0 items-start">
+            <DataCardHeader className="flex flex-row items-center gap-3 justify-start">
+              <div className="w-16 h-16 p-2.5 rounded-xl bg-blue-100 flex flex-row items-center justify-center">
+                <MarsIcon className="text-blue-600 size-9" />
+              </div>
+              <div className="flex flex-col gap-1 items-start">
+                <CardTitle>Guru laki-laki</CardTitle>
+                <CardDescription>
+                  Lorem ipsum dolor sit amet consectetur.
+                </CardDescription>
+              </div>
+            </DataCardHeader>
+            <DataCardBody className="p-0 h-full flex flex-col justify-center">
+              <h2 className="text-5xl font-medium text-black">20</h2>
+            </DataCardBody>
+            <DataCardFooter className="p-0 h-fit">
+              <Button className="w-fit h-fit px-6 py-1.5 flex flex-row items-center justify-center gap-1 rounded-full bg-blue-600">
+                <p className="text-sm font-medium text-white">
+                  lihat selengkapnya
+                </p>
+                <ArrowRightIcon className="text-white" />
+              </Button>
+            </DataCardFooter>
+          </DataCard>
+          <DataCard className="w-full h-[250px] flex flex-col gap-0 items-start">
+            <DataCardHeader className="flex flex-row items-center gap-3 justify-start">
+              <div className="w-16 h-16 p-2.5 rounded-xl bg-pink-100 flex flex-row items-center justify-center">
+                <VenusIcon className="text-pink-600 size-9" />
+              </div>
+              <div className="flex flex-col gap-0 items-start">
+                <CardTitle>Guru laki-laki</CardTitle>
+                <CardDescription>
+                  Lorem ipsum dolor sit amet consectetur.
+                </CardDescription>
+              </div>
+            </DataCardHeader>
+            <DataCardBody className="p-0 h-full flex flex-col justify-center">
+              <h2 className="text-5xl font-medium text-black">20</h2>
+            </DataCardBody>
+            <DataCardFooter className="p-0 h-fit">
+              <Button className="w-fit h-fit px-6 py-1.5 flex flex-row items-center justify-center gap-1 rounded-full bg-pink-600">
+                <p className="text-sm font-medium text-white">
+                  lihat selengkapnya
+                </p>
+                <ArrowRightIcon className="text-white" />
+              </Button>
+            </DataCardFooter>
+          </DataCard>
+          <DataCard className="w-full h-[250px] flex flex-col gap-0 items-start">
+            <DataCardHeader className="flex flex-row items-center gap-3 justify-start">
+              <div className="w-16 h-16 p-2.5 rounded-xl bg-yellow-100 flex flex-row items-center justify-center">
+                <Users2Icon className="text-yellow-600 size-9" />
+              </div>
+              <div className="flex flex-col gap-0 items-start">
+                <CardTitle>Guru laki-laki</CardTitle>
+                <CardDescription>
+                  Lorem ipsum dolor sit amet consectetur.
+                </CardDescription>
+              </div>
+            </DataCardHeader>
+            <DataCardBody className="p-0 h-full flex flex-col justify-center">
+              <h2 className="text-5xl font-medium text-black">20</h2>
+            </DataCardBody>
+            <DataCardFooter className="p-0 h-fit">
+              <Button className="w-fit h-fit px-6 py-1.5 flex flex-row items-center justify-center gap-1 rounded-full bg-yellow-600">
+                <p className="text-sm font-medium text-white">
+                  lihat selengkapnya
+                </p>
+                <ArrowRightIcon className="text-white" />
+              </Button>
+            </DataCardFooter>
+          </DataCard>
         </div>
         {/* attendance graph */}
         <Card className="w-full min-h-[300px]"></Card>
