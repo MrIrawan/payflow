e11f2bb252358deb2129cf6c6d89cc732e2b044e
MrIrawan <irawanssfarrel@gmail.com>
2025-12-26 10:29:05 +0700

    feat: add AttendanceTable component to display today's attendance records

--- src/app/(dashboard)/admin/attendance/page.tsx
+++ src/app/(dashboard)/admin/attendance/page.tsx
@@ -1,4 +1,5 @@
 import { AttendanceDataCard } from "@/components/AttendanceDataCard/attendance-data-card"
+import { AttendanceTable } from "@/components/AttendanceTable/attendance-table"
 import { DashboardBreadcrumb } from "@/components/DashboardBreadcrumb/dashboard-breadcrumb"
 import { CardDescription, CardTitle } from "@/components/ui/card"
 import { SidebarTrigger } from "@/components/ui/sidebar"
@@ -13,11 +14,12 @@ export default function AttendancePage() {
                     <div className="w-full flex flex-col gap-6">
                         <div className="flex flex-col gap-1">
                             <CardTitle className="text-2xl font-bold text-black">Teachers Attendance</CardTitle>
-                            <CardDescription className="text-lg font-medium">Analyse attendance record of teachers</CardDescription>
+                            <CardDescription className="text-lg font-medium">Analyse attendance record of teachers in today</CardDescription>
                         </div>
                         <div className="w-full flex flex-row items-center gap-3">
                             <AttendanceDataCard />
                         </div>
+                        <AttendanceTable />
                     </div>
                 </div>
             </div>
---
+++ src/components/AttendanceTable/attendance-table.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { getAllAttendance } from "@/lib/service/getAllAtendance";
+
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { Card } from "../ui/card";
+
+import { DataTable } from "../DataTable/data-table";
+import { GenderOptionsButton } from "../GenderOptionsButton/gender-options-button";
+import { GetAllAttendance } from "@/types/response";
+import { TableColumn } from "@/types/table";
+
+const tableColumn: TableColumn<GetAllAttendance>[] = [
+    { header: "Teacher Name", accessor: "teacher_name" },
+    { header: "Attendance Date", accessor: "attendance_date" },
+    { header: "Check-in Time", accessor: "checkin_time" },
+    { header: "Check-out Time", accessor: "checkout_time" },
+    { header: "Attendance Status", accessor: "attendance_status" },
+]
+
+export function AttendanceTable() {
+    const [todayAttendance, setTodayAttendance] = useState<GetAllAttendance[]>([]);
+
+    useEffect(() => {
+        async function getTodayAttendance() {
+            const result = await getAllAttendance();
+            const todayAttendance = result.data.filter((attendance) => {
+                const currentDate = new Date().toLocaleDateString("id-ID");
+                const currentAttendance = new Date(attendance.attendance_date).toLocaleDateString("id-ID");
+
+                if (currentAttendance === currentDate) {
+                    return attendance;
+                }
+            })
+
+            setTodayAttendance(todayAttendance);
+        }
+
+        getTodayAttendance();
+    }, [])
+
+    console.log(todayAttendance);
+    return (
+        <div className="w-full flex flex-col gap-6 p-3">
+            <Card className="w-full flex flex-row items-end justify-between p-0 shadow-none border-none">
+                <div className="flex flex-row gap-2.5 items-end">
+                    <div className="flex flex-col gap-2.5">
+                        <Label className="font-semibold">search teacher attendance</Label>
+                        <Input
+                            type="text"
+                            placeholder="teacher neame here..."
+                            className="min-w-[300px]"
+                        />
+                    </div>
+                    <GenderOptionsButton />
+                </div>
+            </Card>
+            <DataTable
+                columns={tableColumn}
+                data={todayAttendance}
+            />
+        </div>
+    )
+}
