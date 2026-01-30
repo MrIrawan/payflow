23d64dcf655c4d70c494c7e9329cf90132923df6
MrIrawan <irawanssfarrel@gmail.com>
2026-01-30 18:08:42 +0700

    feat: add deleteAttendanceData function to handle attendance data deletion

---
+++ src/lib/service/deleteAttendanceData.ts
@@ -0,0 +1,21 @@
+import { fetcher } from "../fetcher/fetcher"
+
+export const deleteAttendanceData = async (identifier: string) => {
+    const response = await fetcher(`/attendance/delete/${identifier}`, {
+        method: "DELETE"
+    });
+
+    if (!response.ok) {
+        return {
+            isSuccess: false,
+            message: response.message,
+            status: response.status,
+            raw: response.raw
+        }
+    }
+
+    return {
+        isSuccess: true,
+        data: response.data
+    }
+}
