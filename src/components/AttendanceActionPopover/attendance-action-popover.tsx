c1ff41853b25213dad01c39468b3d6e179652b99
MrIrawan <irawanssfarrel@gmail.com>
2026-01-30 19:23:20 +0700

    feat: implement AlertDialog for attendance deletion confirmation in AttendanceActionPopover

--- src/components/AttendanceActionPopover/attendance-action-popover.tsx
+++ src/components/AttendanceActionPopover/attendance-action-popover.tsx
@@ -1,10 +1,29 @@
 "use client";
 
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 import { Button } from "../ui/button";
 import { CardDescription } from "../ui/card";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogMedia,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 
 import { EllipsisIcon, FilePenLineIcon, Trash2Icon } from "lucide-react";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Spinner } from "../ui/spinner";
+import { deleteAttendanceData } from "@/lib/service/deleteAttendanceData";
+import { toast } from "sonner";
+import { Toaster } from "../Toaster/toaster";
 
 export function AttendanceActionPopover({ attendanceId }: {
     attendanceId: string;
@@ -24,13 +43,62 @@ export function AttendanceActionPopover({ attendanceId }: {
                             <FilePenLineIcon className="text-black" />
                             <p className="text-black text-sm font-semibold">Edit data absensi</p>
                         </Button>
-                        <Button variant={"default"} size={"sm"} className="w-fit h-fit flex flex-row gap-2 p-2 bg-destructive hover:bg-red-800">
-                            <Trash2Icon className="text-white" />
-                            <p className="text-white text-sm font-medium">Hapus data absensi</p>
-                        </Button>
+                        <AlertDialogDestructive attendanceId={attendanceId} />
                     </div>
                 </div>
             </PopoverContent>
         </Popover>
+    )
+}
+
+export function AlertDialogDestructive({ attendanceId }: {
+    attendanceId: string;
+}) {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [isDelete, setIsDelete] = useState<boolean>(false);
+    const router = useRouter();
+
+    if (isDelete === true) {
+        async function deleteAttendance(attendanceId: string) {
+            const response = await deleteAttendanceData(attendanceId)
+
+            if (!response.isSuccess) {
+                toast.custom(() => <Toaster title="Failed to delete attendance" description={response.message} variant="error" />)
+            } else {
+                toast.custom(() => <Toaster title="success to delete attendance" description={`attendance data with id <${attendanceId}>, has already deleted.`} variant="success" />)
+            }
+        }
+
+        deleteAttendance(attendanceId)
+    }
+
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                <Button variant={"default"} size={"sm"} className="w-fit h-fit flex flex-row gap-2 p-2 bg-destructive hover:bg-red-800">
+                    <Trash2Icon className="text-white" />
+                    <p className="text-white text-sm font-medium">Hapus data absensi</p>
+                </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent size="default">
+                <AlertDialogHeader>
+                    <AlertDialogMedia className="bg-destructive/10 text-destructive dark:bg-destructive/20 dark:text-destructive">
+                        <Trash2Icon />
+                    </AlertDialogMedia>
+                    <AlertDialogTitle>Delete attendance?</AlertDialogTitle>
+                    <AlertDialogDescription className="font-medium">
+                        This will permanently delete this attendance data. are you sure to choose delete option?
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel variant="outline" className="w-20" onClick={() => setIsDelete(false)}>
+                        <p className="text-sm font-medium">Cancel</p>
+                    </AlertDialogCancel>
+                    <AlertDialogAction variant="destructive" className="w-20" onClick={() => setIsDelete(true)}>
+                        {isLoading ? (<Spinner className="size-3.5 text-white" />) : (<p className="text-sm font-medium">Delete</p>)}
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
     )
 }
