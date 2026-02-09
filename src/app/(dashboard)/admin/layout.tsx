b76be8a1ea8c5ba11a0fafe7664fad544dee61bd
MrIrawan <irawanssfarrel@gmail.com>
2026-02-09 19:12:19 +0700

    feat: applying validation token for admin and employee page

--- src/app/(dashboard)/admin/layout.tsx
+++ src/app/(dashboard)/admin/layout.tsx
@@ -1,9 +1,18 @@
+import { getCookie } from "@/utils/getCookie";
+import { redirect } from "next/navigation";
+
 import { AdminSidebar } from "@/components/AdminSidebar/admin-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 
-export default function AdminLayout({
+export default async function AdminLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const isAdminToken = await getCookie("admin_token");
+
+  if (!isAdminToken) {
+    redirect("/admin/signIn");
+  }
+
   return (
     <SidebarProvider>
       <AdminSidebar />
--- src/app/(dashboard)/employee/layout.tsx
+++ src/app/(dashboard)/employee/layout.tsx
@@ -1,7 +1,16 @@
+import { getCookie } from "@/utils/getCookie";
+import { redirect } from "next/navigation";
+
 import { EmployeeSidebar } from "@/components/EmployeeSidebar/employee-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 
-export default function EmployeePageLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default async function EmployeePageLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+    const isRefreshToken = await getCookie("refreshToken");
+
+    if (!isRefreshToken) {
+        redirect("/signIn");
+    }
+
     return (
         <SidebarProvider>
             <EmployeeSidebar />
