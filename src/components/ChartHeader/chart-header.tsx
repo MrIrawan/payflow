93ed7e8402ca3a4eabc416e0bf63fe161ec81fd4
MrIrawan <irawanssfarrel@gmail.com>
2026-02-10 16:39:47 +0700

    feat: adding card component as a wrapper to employee attendance chart and chart header

--- src/components/ChartHeader/chart-header.tsx
+++ src/components/ChartHeader/chart-header.tsx
@@ -13,7 +13,9 @@ import {
   SelectValue,
 } from "../ui/select";
 
-export function ChartHeader() {
+export function ChartHeader({ withMonthAndYear = false }: {
+  withMonthAndYear?: boolean;
+}) {
   // monthState build with useState to catch month state
   const [monthState, setMonthState] = useState<string | null>(null);
   // yearState build with useState to catch year state
@@ -38,100 +40,103 @@ export function ChartHeader() {
         <CardDescription>Based on Attendance data</CardDescription>
       </CardHeader>
       <div className="flex flex-row justify-between items-center gap-4">
-        {/* month select component */}
-        <Select onValueChange={(month) => setMonthState(month)}>
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="month" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectLabel>years</SelectLabel>
-              <SelectItem value="january" className="capitalize">
-                january
-              </SelectItem>
-              <SelectItem value="fabruary" className="capitalize">
-                February
-              </SelectItem>
-              <SelectItem value="march" className="capitalize">
-                March
-              </SelectItem>
-              <SelectItem value="april" className="capitalize">
-                April
-              </SelectItem>
-              <SelectItem value="may" className="capitalize">
-                May
-              </SelectItem>
-              <SelectItem value="june" className="capitalize">
-                June
-              </SelectItem>
-              <SelectItem value="july" className="capitalize">
-                july
-              </SelectItem>
-              <SelectItem value="august" className="capitalize">
-                august
-              </SelectItem>
-              <SelectItem value="september" className="capitalize">
-                september
-              </SelectItem>
-              <SelectItem value="october" className="capitalize">
-                october
-              </SelectItem>
-              <SelectItem value="november" className="capitalize">
-                november
-              </SelectItem>
-              <SelectItem value="december" className="capitalize">
-                december
-              </SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-        {/* year select component */}
-        <Select onValueChange={(year) => setYearState(year)}>
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="year" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectLabel>Months</SelectLabel>
-              <SelectItem value="2025" className="capitalize">
-                2025
-              </SelectItem>
-              <SelectItem value="2024" className="capitalize">
-                2024
-              </SelectItem>
-              <SelectItem value="2023" className="capitalize">
-                2023
-              </SelectItem>
-              <SelectItem value="2022" className="capitalize">
-                2022
-              </SelectItem>
-              <SelectItem value="2021" className="capitalize">
-                2021
-              </SelectItem>
-              <SelectItem value="2020" className="capitalize">
-                2020
-              </SelectItem>
-              <SelectItem value="2019" className="capitalize">
-                2019
-              </SelectItem>
-              <SelectItem value="2018" className="capitalize">
-                2018
-              </SelectItem>
-              <SelectItem value="2017" className="capitalize">
-                2017
-              </SelectItem>
-              <SelectItem value="2016" className="capitalize">
-                2016
-              </SelectItem>
-              <SelectItem value="2015" className="capitalize">
-                2015
-              </SelectItem>
-              <SelectItem value="2014" className="capitalize">
-                2014
-              </SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
+        {/* month and year select component */}
+        {withMonthAndYear && (
+          <>
+            <Select onValueChange={(month) => setMonthState(month)}>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="month" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>years</SelectLabel>
+                  <SelectItem value="january" className="capitalize">
+                    january
+                  </SelectItem>
+                  <SelectItem value="fabruary" className="capitalize">
+                    February
+                  </SelectItem>
+                  <SelectItem value="march" className="capitalize">
+                    March
+                  </SelectItem>
+                  <SelectItem value="april" className="capitalize">
+                    April
+                  </SelectItem>
+                  <SelectItem value="may" className="capitalize">
+                    May
+                  </SelectItem>
+                  <SelectItem value="june" className="capitalize">
+                    June
+                  </SelectItem>
+                  <SelectItem value="july" className="capitalize">
+                    july
+                  </SelectItem>
+                  <SelectItem value="august" className="capitalize">
+                    august
+                  </SelectItem>
+                  <SelectItem value="september" className="capitalize">
+                    september
+                  </SelectItem>
+                  <SelectItem value="october" className="capitalize">
+                    october
+                  </SelectItem>
+                  <SelectItem value="november" className="capitalize">
+                    november
+                  </SelectItem>
+                  <SelectItem value="december" className="capitalize">
+                    december
+                  </SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+            <Select onValueChange={(year) => setYearState(year)}>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="year" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Months</SelectLabel>
+                  <SelectItem value="2025" className="capitalize">
+                    2025
+                  </SelectItem>
+                  <SelectItem value="2024" className="capitalize">
+                    2024
+                  </SelectItem>
+                  <SelectItem value="2023" className="capitalize">
+                    2023
+                  </SelectItem>
+                  <SelectItem value="2022" className="capitalize">
+                    2022
+                  </SelectItem>
+                  <SelectItem value="2021" className="capitalize">
+                    2021
+                  </SelectItem>
+                  <SelectItem value="2020" className="capitalize">
+                    2020
+                  </SelectItem>
+                  <SelectItem value="2019" className="capitalize">
+                    2019
+                  </SelectItem>
+                  <SelectItem value="2018" className="capitalize">
+                    2018
+                  </SelectItem>
+                  <SelectItem value="2017" className="capitalize">
+                    2017
+                  </SelectItem>
+                  <SelectItem value="2016" className="capitalize">
+                    2016
+                  </SelectItem>
+                  <SelectItem value="2015" className="capitalize">
+                    2015
+                  </SelectItem>
+                  <SelectItem value="2014" className="capitalize">
+                    2014
+                  </SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </>
+        )}
       </div>
     </Card>
   );
--- src/components/EmployeeAttendanceGraph/employee-attendance-graph.tsx
+++ src/components/EmployeeAttendanceGraph/employee-attendance-graph.tsx
@@ -7,21 +7,8 @@ import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
 
 import { ChartLegend, ChartLegendContent, type ChartConfig } from "@/components/ui/chart"
 import { ChartContainer, ChartTooltip, ChartTooltipContent } from "../ui/chart";
-
-const chartData = [
-    { month: "Januari", present: 20, absent: 0, onLeave: 10 },
-    { month: "Februari", present: 15, absent: 10, onLeave: 5 },
-    { month: "Maret", present: 10, absent: 5, onLeave: 15 },
-    { month: "April", present: 10, absent: 5, onLeave: 5 },
-    { month: "Mei", present: 15, absent: 3, onLeave: 2 },
-    { month: "Juni", present: 0, absent: 0, onLeave: 0 },
-    { month: "Juli", present: 0, absent: 0, onLeave: 0 },
-    { month: "Agustus", present: 0, absent: 0, onLeave: 0 },
-    { month: "September", present: 0, absent: 0, onLeave: 0 },
-    { month: "Oktober", present: 0, absent: 0, onLeave: 0 },
-    { month: "November", present: 0, absent: 0, onLeave: 0 },
-    { month: "Desember", present: 0, absent: 0, onLeave: 0 },
-]
+import { Card } from "../ui/card";
+import { ChartHeader } from "../ChartHeader/chart-header";
 
 const chartConfig = {
     present: {
@@ -44,22 +31,25 @@ export function EmployeeAttendanceGraph({ attendanceChartData }: {
     const [attendanceData, setAttendanceData] = useState<AttendanceChartItem[] | undefined>(attendanceChartData || undefined);
 
     return (
-        <ChartContainer config={chartConfig} className="h-[500px] w-full">
-            <BarChart accessibilityLayer data={attendanceData}>
-                <CartesianGrid vertical={false} />
-                <XAxis
-                    dataKey="month"
-                    tickLine={false}
-                    tickMargin={10}
-                    axisLine={false}
-                    tickFormatter={(value) => value.slice(0, 3)}
-                />
-                <ChartTooltip content={<ChartTooltipContent />} />
-                <ChartLegend content={<ChartLegendContent />} />
-                <Bar dataKey="present" fill="var(--color-present)" radius={4} />
-                <Bar dataKey="absent" fill="var(--color-absent)" radius={4} />
-                <Bar dataKey="onLeave" fill="var(--color-onLeave)" radius={4} />
-            </BarChart>
-        </ChartContainer>
+        <Card className="shadow-none">
+            <ChartHeader />
+            <ChartContainer config={chartConfig} className="h-[500px] w-full">
+                <BarChart accessibilityLayer data={attendanceData}>
+                    <CartesianGrid vertical={false} />
+                    <XAxis
+                        dataKey="month"
+                        tickLine={false}
+                        tickMargin={10}
+                        axisLine={false}
+                        tickFormatter={(value) => value.slice(0, 3)}
+                    />
+                    <ChartTooltip content={<ChartTooltipContent />} />
+                    <ChartLegend content={<ChartLegendContent />} />
+                    <Bar dataKey="present" fill="var(--color-present)" radius={4} />
+                    <Bar dataKey="absent" fill="var(--color-absent)" radius={4} />
+                    <Bar dataKey="onLeave" fill="var(--color-onLeave)" radius={4} />
+                </BarChart>
+            </ChartContainer>
+        </Card>
     )
 }
