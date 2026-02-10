746e7fdc1156bf3f6243069d07b916941e1f3196
MrIrawan <irawanssfarrel@gmail.com>
2026-02-10 18:20:21 +0700

    feat: data mapping on teacher profile page

--- src/app/(dashboard)/employee/me/page.tsx
+++ src/app/(dashboard)/employee/me/page.tsx
@@ -1,11 +1,37 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { getUserProfile } from "@/lib/service/user/profile/getUserProfile";
+import { DashboardData } from "@/types/response";
+
 import { DashboardBreadcrumb } from "@/components/DashboardBreadcrumb/dashboard-breadcrumb";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Card } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Loader2 } from "lucide-react";
+import { toast } from "sonner";
+import { Toaster } from "@/components/Toaster/toaster";
+import { GenderBadge } from "@/components/GenderBadge/gender-badge";
 
 export default function EmployeePage() {
+    const [data, setData] = useState<DashboardData | undefined>(undefined);
+
+    useEffect(() => {
+        async function fetchUserProfile() {
+            const response = await getUserProfile();
+
+            if (!response.isSuccess) {
+                toast.custom(() => <Toaster variant="error" title="gagal mendapatkan profile kamu." description={response.message} />);
+                return;
+            }
+
+            setData(response.data?.data);
+        }
+
+        fetchUserProfile();
+    }, []);
+
     return (
         <>
             <section className="w-full p-6">
@@ -17,10 +43,12 @@ export default function EmployeePage() {
                     </div>
                     {/* content area */}
                     <Card className="w-full h-[300px] shadow-none flex flex-col items-center gap-3">
-                        <Avatar className="w-32 h-32 ring-pink-600 ring-[2px]">
-                            <AvatarFallback className="bg-pink-50 text-4xl font-semibold text-pink-600">AV</AvatarFallback>
+                        <Avatar className={`w-32 h-32 ${data?.profile.gender ? (data?.profile.gender === "male" ? "ring-blue-600" : "ring-pink-600") : "ring-gray-600"} ring-[2px]`}>
+                            <AvatarFallback className={`${data?.profile.gender ? (data?.profile.gender === "male" ? "bg-blue-50 text-blue-600" : "bg-pink-50 text-pink-600") : "bg-gray-50 text-gray-600"} text-4xl font-semibold uppercase`}>
+                                {data?.profile.full_name.slice(0, 2)}
+                            </AvatarFallback>
                         </Avatar>
-                        <h2 className="text-4xl font-semibold text-black">Kiki Rosiana</h2>
+                        <h2 className="text-4xl font-semibold text-black capitalize">{data?.profile.full_name || "User Full Name"}</h2>
                         <span className="w-fit flex flex-row gap-3">
                             <span className="w-fit h-fit px-3 py-1 rounded-xl bg-blue-50 ring-blue-600 ring-1">
                                 <p className="text-xs font-medium text-blue-600">hanjayy</p>
@@ -49,19 +77,25 @@ export default function EmployeePage() {
                                 <ul className="w-full flex flex-col gap-3">
                                     <li className="flex flex-col gap-0">
                                         <h3 className="text-sm font-semibold text-muted-foreground">Nama Lengkap</h3>
-                                        <p className="text-lg font-semibold text-black">Lorem ipsum dolor sit amet.</p>
+                                        <p className="text-lg font-semibold text-black">{data?.profile.full_name || "Tidak diketahui"}</p>
                                     </li>
                                     <li className="flex flex-col gap-0">
                                         <h3 className="text-sm font-semibold text-muted-foreground">Tanggal Lahir</h3>
-                                        <p className="text-lg font-semibold text-black">Lorem ipsum dolor sit amet.</p>
+                                        <p className="text-lg font-semibold text-black">
+                                            {new Date(data?.profile.date_of_birth || "").toLocaleDateString("id-ID", {
+                                                month: "long",
+                                                day: "numeric",
+                                                year: "numeric"
+                                            }) || "Tidak diketahui"}
+                                        </p>
                                     </li>
                                     <li className="flex flex-col gap-0">
                                         <h3 className="text-sm font-semibold text-muted-foreground">Jenis Kelamin</h3>
-                                        <p className="text-lg font-semibold text-black">Lorem ipsum dolor sit amet.</p>
+                                        <p className="text-lg font-semibold text-black">{data?.profile.gender ? (data.profile.gender === "male" ? (<GenderBadge placeholder={"male"} size="sm" />) : (<GenderBadge placeholder={"female"} size="sm" />)) : "Tidak diketahui"}</p>
                                     </li>
                                     <li className="flex flex-col gap-0">
                                         <h3 className="text-sm font-semibold text-muted-foreground">Alamat Rumah</h3>
-                                        <p className="text-lg font-semibold text-black">Lorem ipsum dolor sit amet.</p>
+                                        <p className="text-lg font-semibold text-black">{data?.profile.home_address || "Tidak diketahui"}</p>
                                     </li>
                                 </ul>
                             </div>
@@ -73,19 +107,21 @@ export default function EmployeePage() {
                                 <ul className="w-full flex flex-col gap-3">
                                     <li className="flex flex-col gap-0">
                                         <h3 className="text-sm font-semibold text-muted-foreground">Nama Jabatan</h3>
-                                        <p className="text-lg font-semibold text-black">Lorem ipsum dolor sit amet.</p>
+                                        <p className="text-lg font-semibold text-black">{data?.profile.job_title || "Tidak diketahui"}</p>
                                     </li>
                                     <li className="flex flex-col gap-0">
                                         <h3 className="text-sm font-semibold text-muted-foreground">Nama Perusahaan</h3>
-                                        <p className="text-lg font-semibold text-black">Lorem ipsum dolor sit amet.</p>
+                                        <p className="text-lg font-semibold text-black">{data?.profile.company}</p>
                                     </li>
                                     <li className="flex flex-col gap-0">
                                         <h3 className="text-sm font-semibold text-muted-foreground">Mata Pelajaran</h3>
-                                        <p className="text-lg font-semibold text-black">Lorem ipsum dolor sit amet.</p>
+                                        <p className="text-lg font-semibold text-black">{data?.profile.subject_name || "Tidak diketahui"}</p>
                                     </li>
                                     <li className="flex flex-col gap-0">
                                         <h3 className="text-sm font-semibold text-muted-foreground">Tanggal Bergabung</h3>
-                                        <p className="text-lg font-semibold text-black">Lorem ipsum dolor sit amet.</p>
+                                        <p className="text-lg font-semibold text-black">
+                                            {data?.profile.join_date.split("T")[0] || "Tidak diketahui"}
+                                        </p>
                                     </li>
                                 </ul>
                             </div>
@@ -96,19 +132,19 @@ export default function EmployeePage() {
                         <div className="w-full grid grid-cols-4 gap-3">
                             <div className="flex flex-col gap-0">
                                 <h3 className="text-base font-medium text-muted-foreground">Nama Perusahaan</h3>
-                                <p className="text-black text-lg font-semibold">SMK Nurjamilah</p>
+                                <p className="text-black text-lg font-semibold">{data?.profile.company || "Tidak diketahui"}</p>
                             </div>
                             <div className="flex flex-col gap-0">
                                 <h3 className="text-base font-medium text-muted-foreground">Alamat Email</h3>
-                                <p className="text-black text-lg font-semibold">kikirosiana@email.com</p>
+                                <p className="text-black text-lg font-semibold">{data?.profile.email_address || "Tidak diketahui"}</p>
                             </div>
                             <div className="flex flex-col gap-0">
                                 <h3 className="text-base font-medium text-muted-foreground">Tanggal Bergabung</h3>
-                                <p className="text-black text-lg font-semibold">17 Februari 2015</p>
+                                <p className="text-black text-lg font-semibold">{data?.profile.join_date.split("T")[0] || "Tidak diketahui"}</p>
                             </div>
                             <div className="flex flex-col gap-0">
                                 <h3 className="text-base font-medium text-muted-foreground">Nama Jabatan</h3>
-                                <p className="text-black text-lg font-semibold">Guru dan Kesiswaan</p>
+                                <p className="text-black text-lg font-semibold">{data?.profile.job_title || "Tidak diketahui"}</p>
                             </div>
                         </div>
                     </Card>
