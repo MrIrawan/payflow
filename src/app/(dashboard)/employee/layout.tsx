45d84e72b8448bd0d6bdbba1511f44d422fe15bd
MrIrawan <irawanssfarrel@gmail.com>
2026-02-11 20:04:06 +0700

    feat: adding redirect validation on employee layout page

--- src/app/(dashboard)/employee/layout.tsx
+++ src/app/(dashboard)/employee/layout.tsx
@@ -1,7 +1,17 @@
+import { getCookie } from "@/utils/getCookie";
+
 import { EmployeeSidebar } from "@/components/EmployeeSidebar/employee-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { redirect } from "next/navigation";
 
-export default function EmployeePageLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default async function EmployeePageLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+
+    const refreshToken = await getCookie("refreshToken");
+
+    if (!refreshToken) {
+        redirect("/signIn");
+    }
+
     return (
         <SidebarProvider>
             <EmployeeSidebar />
