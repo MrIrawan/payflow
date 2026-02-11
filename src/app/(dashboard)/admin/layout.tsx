9314e2f693ba4a0534aae568adb33a33f2ed1257
MrIrawan <irawanssfarrel@gmail.com>
2026-02-11 20:01:57 +0700

    feat: adding redirect validation on admin layout page

--- src/app/(dashboard)/admin/layout.tsx
+++ src/app/(dashboard)/admin/layout.tsx
@@ -1,9 +1,19 @@
+import { getCookie } from "@/utils/getCookie";
+
 import { AdminSidebar } from "@/components/AdminSidebar/admin-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { redirect } from "next/navigation";
 
-export default function AdminLayout({
+export default async function AdminLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+
+  const adminToken = await getCookie("admin_token");
+
+  if (!adminToken) {
+    redirect("/admin/signIn");
+  }
+
   return (
     <SidebarProvider>
       <AdminSidebar />
