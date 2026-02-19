1f494a337a379f3ee7fbebd13df71db852d3359b
MrIrawan <irawanssfarrel@gmail.com>
2026-02-19 22:38:16 +0700

    feat: integrate employee register page with react hook form

--- src/app/(auth)/signUp/page.tsx
+++ src/app/(auth)/signUp/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import React, { useState } from 'react';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
+import { SignUpRequest } from '@/types/request';
+import { signUpEmployee } from '@/lib/services/signUpEmployee';
+
 import Link from 'next/link';
 import Image from 'next/image';
 
-import PayFlowLogoWithTitle from "../../../../public/images/payflow_logo_with_title.svg";
-
-import { UserPlus, Shield, Venus, Mars } from 'lucide-react';
+import { Venus, Mars } from 'lucide-react';
 import { InputGroup } from '@/components/InputGroup/input-group';
 import { DatePicker } from '@/components/DatePicker/date-picker';
 import { FormComponent, FormContent, FormFooter } from '@/components/Form/Form';
@@ -15,7 +17,12 @@ import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 
 export default function RegisterPage() {
-  const [selectedGender, setSelectedGender] = useState<"male" | "female" | null>(null);
+  const { register, handleSubmit, control, formState: { errors }, watch } = useForm<SignUpRequest>();
+  const selectedGender = watch("gender");
+
+  const onSubmit: SubmitHandler<SignUpRequest> = async (data) => {
+    console.log(data);
+  }
 
   return (
     <>
@@ -44,7 +51,7 @@ export default function RegisterPage() {
             </p>
           </div>
 
-          <FormComponent asWrapper={false} className='flex flex-col gap-8'>
+          <FormComponent asWrapper={false} className='flex flex-col gap-8' onSubmit={handleSubmit(onSubmit)}>
             <FormContent className='flex flex-col gap-4'>
               {/* first name and last name */}
               <div className='flex flex-row justify-between items-start gap-3'>
@@ -53,19 +60,45 @@ export default function RegisterPage() {
                   placeholder='Jhon'
                   label='First Name'
                   htmlFor='first_name'
+                  aria-invalid={errors.first_name ? "true" : "false"}
+                  errorMsg={errors.first_name?.message}
+                  {...register("first_name", {
+                    required: {
+                      value: true, message: "nama depan wajib di isi."
+                    }, minLength: {
+                      value: 3, message: "panjang nama depan minimal 3 huruf."
+                    }
+                  })}
                 />
                 <InputGroup
                   type='text'
                   placeholder='Doe'
                   label='Last Name'
                   htmlFor='last_name'
+                  aria-invalid={errors.last_name ? "true" : "false"}
+                  errorMsg={errors.last_name?.message}
+                  {...register("last_name", {
+                    minLength: {
+                      value: 3,
+                      message: "panjang nama belakang minimal 3 huruf."
+                    }
+                  })}
                 />
               </div>
               {/* date of birth */}
-              <DatePicker
-                label='Date of Birth'
-                htmlFor='date_of_birth'
-                placeholder='Select date'
+              <Controller
+                control={control}
+                name='date_of_birth'
+                render={({ field }) => (
+                  <DatePicker
+                    label='Date of Birth'
+                    htmlFor='date_of_birth'
+                    placeholder='Select date'
+                    value={field.value}
+                    onChange={field.onChange}
+                    requiredLabel
+                  />
+                )}
               />
               {/* gender radio options */}
               <div className='flex flex-col gap-2.5'>
@@ -75,11 +108,14 @@ export default function RegisterPage() {
                     <Input
                       type='radio'
                       id='gender'
-                      name='gender'
                       value={"male"}
                       className='hidden'
-                      checked={selectedGender === "male"}
-                      onChange={(e) => setSelectedGender(e.target.value as "male")}
+                      {...register("gender", {
+                        required: {
+                          value: true,
+                          message: "jenis kelamin wajib di isi."
+                        }
+                      })}
                     />
                     <Mars className={`${selectedGender === "male" ? "text-blue-600" : "text-muted-foreground"} size-7`} />
                     <p className={`${selectedGender === "male" ? "text-blue-600" : "text-muted-foreground"} text-sm font-medium`}>Laki-laki</p>
@@ -88,11 +124,14 @@ export default function RegisterPage() {
                     <Input
                       type='radio'
                       id='gender'
-                      name='gender'
                       value={"female"}
                       className='hidden'
-                      checked={selectedGender === "female"}
-                      onChange={(e) => setSelectedGender(e.target.value as "female")}
+                      {...register("gender", {
+                        required: {
+                          value: true,
+                          message: "jenis kelamin wajib di isi."
+                        }
+                      })}
                     />
                     <Venus className={`${selectedGender === "female" ? "text-pink-600" : "text-muted-foreground"} size-7`} />
                     <p className={`text-sm font-medium ${selectedGender === "female" ? "text-pink-600" : "text-muted-foreground"}`}>Perempuan</p>
@@ -105,13 +144,33 @@ export default function RegisterPage() {
                 placeholder='jhondoe@mail.com'
                 label='Email Address'
                 htmlFor='email_address'
+                aria-invalid={errors.email_address ? "true" : "false"}
+                errorMsg={errors.email_address?.message}
+                {...register("email_address", {
+                  required: {
+                    value: true,
+                    message: "alamat email wajib di isi."
+                  }
+                })}
               />
               {/* password email */}
               <InputGroup
                 type='password'
                 placeholder='********'
-                label='Password EMail'
+                label='Password Email'
                 htmlFor='password_email'
+                aria-invalid={errors.password_email ? "true" : "false"}
+                errorMsg={errors.password_email?.message}
+                {...register("password_email", {
+                  required: {
+                    value: true,
+                    message: "password email wajib di isi."
+                  },
+                  minLength: {
+                    value: 5,
+                    message: "panjang password email minimal 5 huruf."
+                  }
+                })}
               />
             </FormContent>
             <FormFooter>
