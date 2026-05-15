fc6465b9d1252f05eab89a7fe7105a3855fbadf1
MrIrawan <irawanssfarrel@gmail.com>
2026-05-15 17:12:26 +0700

    feat: enhance EmployeeProfileCard with dynamic avatar, employee details, and action button

--- src/components/EmployeeProfileCard/employee-profile-card.tsx
+++ src/components/EmployeeProfileCard/employee-profile-card.tsx
@@ -3,6 +3,8 @@ import { EmployeeProfileCardProps } from "@/types/types";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Card, CardDescription, CardTitle } from "../ui/card";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import { Building2, CalendarClock, User2 } from "lucide-react";
+import { Button } from "../ui/button";
 
 
 export function EmployeeProfileCard({ employeeData }: {
@@ -16,7 +18,7 @@ export function EmployeeProfileCard({ employeeData }: {
                         <Card className="w-full h-14 p-2 shadow-none transition-all hover:bg-gray-100/50 cursor-pointer">
                             <div className="w-full h-full flex flex-row items-center justify-start gap-2">
                                 <Avatar className="w-[15%] h-full rounded-full">
-                                    <AvatarFallback className="h-full rounded-full">AV</AvatarFallback>
+                                    <AvatarFallback className={`h-full rounded-full font-semibold text-white text-sm ${data.gender === "male" ? "bg-blue-500" : "bg-pink-500"}`}>{data.full_name.slice(0, 2)}</AvatarFallback>
                                     <AvatarImage />
                                 </Avatar>
                                 <div className="w-full h-full flex flex-col items-start justify-start gap-0">
@@ -26,15 +28,46 @@ export function EmployeeProfileCard({ employeeData }: {
                             </div>
                         </Card>
                     </DropdownMenuTrigger>
-                    <DropdownMenuContent side="left" align="start" className="w-96 h-96">
-                        <div className="w-full h-2/6 bg-red-200">
+                    <DropdownMenuContent side="left" align="start" className="w-96 h-96 p-4 flex flex-col gap-2">
+                        <div className="w-full h-2/6">
                             <Avatar className="h-full w-2/6">
-                                <AvatarFallback className="h-full">AV</AvatarFallback>
+                                <AvatarFallback className={`h-full font-semibold text-3xl text-white ${data.gender === "male" ? "bg-blue-500" : "bg-pink-500"}`}>{data.full_name.slice(0, 2)}</AvatarFallback>
                             </Avatar>
                         </div>
-                        <div className="w-full h-2/12 bg-green-300"></div>
-                        <div className="w-full"></div>
-                        <div className="w-full"></div>
+                        <div className="w-full h-2/12 flex flex-col gap-0">
+                            <h2 className="font-semibold text-black text-xl">{data.full_name}</h2>
+                            <h3 className="font-medium text-muted-foreground text-base">{data.email}</h3>
+                        </div>
+                        <div className="w-full flex flex-col gap-4">
+                            <div className="flex flex-row items-center justify-start gap-2">
+                                <div className="rounded-sm w-10 h-10 bg-blue-100 flex flex-row items-center justify-center">
+                                    <Building2 className="text-blue-500 size-5" />
+                                </div>
+                                <div className="flex flex-col gap-0">
+                                    <CardTitle className="text-sm font-semibold">Company Name</CardTitle>
+                                    <CardDescription className="text-xs font-medium">{data.company_id}</CardDescription>
+                                </div>
+                            </div>
+                            <div className="flex flex-row items-center justify-start gap-2">
+                                <div className="rounded-sm w-10 h-10 bg-green-100 flex flex-row items-center justify-center">
+                                    <CalendarClock className="text-green-500 size-5" />
+                                </div>
+                                <div className="flex flex-col gap-0">
+                                    <CardTitle className="text-sm font-semibold">Join Date</CardTitle>
+                                    <CardDescription className="text-xs font-medium">{new Date(data.join_date).toLocaleDateString("id-ID", {
+                                        day: "numeric",
+                                        month: "long",
+                                        year: "numeric"
+                                    })}</CardDescription>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-full">
+                            <Button className="bg-blue-500 hover:bg-blue-700 [&_svg:not([class*='size-'])]:size-5">
+                                <User2 fill="white" strokeWidth={1} />
+                                <p className="text-sm font-medium text-white">See profile</p>
+                            </Button>
+                        </div>
                     </DropdownMenuContent>
                 </DropdownMenu>
             ))}
