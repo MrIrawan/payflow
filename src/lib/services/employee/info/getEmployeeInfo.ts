ae565ea35566edb5abb3b396d771212dc4a4bfe4
MrIrawan <irawanssfarrel@gmail.com>
2026-05-30 16:02:37 +0700

    feat: add employee info and profile service modules to fetch data from API

--- src/lib/services/employee/info/getEmployeeInfo.ts
+++ src/lib/services/employee/info/getEmployeeInfo.ts
@@ -1,23 +1,23 @@
 import userClient from "@/lib/axios/userClient";
-
 import { ApiResponse } from "@/types/api";
-import { GetEmployeeInfoData, GetEmployeeInfoResponse } from "@/types/response";
+import { GetEmployeeInfoData } from "@/types/response";
 
 export const getEmployeeInfo = async (companyId: number) => {
     try {
-        const response = await userClient.get<ApiResponse<GetEmployeeInfoData>>(`/info?companyId=${companyId}`);
+        const response = await userClient.get<ApiResponse<GetEmployeeInfoData>>(
+            `/info?companyId=${companyId}`
+        );
 
         return {
             success: response.data.success,
             message: response.data.message,
             data: response.data.data,
-        }
-
-    } catch (error: ApiResponse<GetEmployeeInfoResponse> | any) {
+        };
+    } catch {
         return {
             success: false,
             message: "gagal mengambil info dashboard pegawai",
             data: null,
-        }
+        };
     }
-}
+};
