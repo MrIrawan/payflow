1a8919e1d25253c5faa96169323cc3ad055505a4
MrIrawan <irawanssfarrel@gmail.com>
2026-07-19 16:59:47 +0700

    refactor: separate employee sidebar and total employee sidebar component into employee right and left folder

---
+++ src/components/EmployeeLeftSidebar/employee-left-sidebar.tsx
@@ -0,0 +1,231 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
+
+import { getEmployeeProfile } from "@/lib/services/employee/profile/getEmployeeProfile";
+import { GetEmployeeProfileData } from "@/types/response";
+import { logOutEmployee } from "@/lib/services/employee/auth/logOutEmployee";
+
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+import LogoWithTitle from "../../../public/images/payflow_logo_with_title.svg"
+
+import { toast } from "sonner";
+import { Toaster } from "../Toaster/toaster";
+
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem
+} from "../ui/sidebar";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import { Avatar, AvatarFallback } from "../ui/avatar";
+import { Separator } from "../ui/separator";
+import { Button } from "../ui/button";
+import { Skeleton } from "../ui/skeleton";
+import { Spinner } from "../ui/spinner";
+
+import { CollabsipleSidebarNavigation } from "../CollapsibleSidebarNavigation/collapsible-sidebar-navigation";
+import { SidebarNavigationLink } from "../SidebarNavigationLink/sidebar-navigation-link";
+
+import { CalendarCheck2, HomeIcon, House, LogOut, ReceiptText, UserCircleIcon, Users, Wallet, WalletIcon } from "lucide-react";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogMedia, AlertDialogTitle, AlertDialogTrigger } from "../ui/alert-dialog";
+
+export function EmployeeSidebar() {
+    const [employeeProfile, setEmployeeProfile] = useState<GetEmployeeProfileData | undefined>(undefined);
+    const params = useParams();
+
+    const companyId = Number(params.companyId);
+
+    useEffect(() => {
+        async function fetchEmployeeProfile() {
+            const response = await getEmployeeProfile(companyId);
+
+            if (response.success === false) {
+                console.error("gagal mengambil data profile pegawai:", response.message);
+                return;
+            }
+
+            if (response.data !== null) {
+                setEmployeeProfile(response.data);
+            }
+        };
+
+        fetchEmployeeProfile();
+    }, [companyId]);
+    return (
+        <Sidebar className="px-2.5">
+            <SidebarHeader className="p-4">
+                <Image src={LogoWithTitle} alt="payflow-logo" width={200} />
+            </SidebarHeader>
+            <SidebarContent>
+                <SidebarGroup className="h-full">
+                    {/* Sidebar items go here */}
+                    <SidebarMenu className="gap-4">
+                        <SidebarNavigationLink
+                            href={`/employee/${companyId}`}
+                            label="dashboard"
+                            Icon={House}
+                            activeBg
+                        />
+                        <SidebarNavigationLink
+                            href={`/employee/${companyId}/me`}
+                            label="Profile anda"
+                            Icon={Users}
+                            activeBg
+                        />
+                        <CollabsipleSidebarNavigation
+                            label="Penggajian"
+                            Icon={Wallet}
+                            sub={[
+                                { label: "Estimasi Gaji", href: `/employee/${companyId}/payroll/live` },
+                                { label: "Riwayat Gaji", href: `/employee/${companyId}/payroll/history` },
+                            ]}
+                        />
+                        <SidebarNavigationLink
+                            href={`/employee/${companyId}/attendance`}
+                            label="absensi mandiri"
+                            Icon={CalendarCheck2}
+                            activeBg
+                        />
+                    </SidebarMenu>
+                </SidebarGroup>
+            </SidebarContent>
+            <Separator />
+            <SidebarFooter>
+                <SidebarMenu>
+                    {employeeProfile === undefined ? (
+                        <Skeleton className="w-full h-[55px] bg-gray-300" />
+                    ) : (
+                        <SidebarMenuItem>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger asChild>
+                                    <SidebarMenuButton className="flex flex-row items-center gap-2 w-full h-fit">
+                                        <Avatar className="w-10 h-10 rounded-md">
+                                            <AvatarFallback className={`rounded-md text-white font-medium ${employeeProfile.gender === "male" ? "bg-blue-600" : "bg-pink-600"}`}>{employeeProfile.full_name.slice(0, 2)}</AvatarFallback>
+                                        </Avatar>
+                                        <div className="flex flex-col">
+                                            <p className="text-sm font-medium text-black">{employeeProfile.full_name}</p>
+                                            <p className="text-xs font-medium text-muted-foreground">{employeeProfile.email}</p>
+                                        </div>
+                                    </SidebarMenuButton>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent side="right" className="min-w-[250px] flex flex-col justify-between h-fit">
+                                    <div className="w-full flex flex-row items-center gap-2 p-2">
+                                        <Avatar className="w-10 h-10 rounded-md">
+                                            <AvatarFallback className={`rounded-md text-white font-medium ${employeeProfile.gender === "male" ? "bg-blue-600" : "bg-pink-600"}`}>{employeeProfile.full_name.slice(0, 2)}</AvatarFallback>
+                                        </Avatar>
+                                        <div className="w-full flex flex-col">
+                                            <p className="text-sm font-medium text-black">{employeeProfile.full_name}</p>
+                                            <p className="text-xs font-medium text-muted-foreground">{employeeProfile.email}</p>
+                                        </div>
+                                    </div>
+                                    <Separator />
+                                    <div className="w-full flex flex-col gap-0 py-1">
+                                        <Link href={`/employee/${companyId}`}>
+                                            <Button variant={"ghost"} className="w-full flex flex-row gap-1 items-center justify-start has-[>svg]:p-2">
+                                                <HomeIcon />
+                                                <p className="text-sm font-medium">Dashboard</p>
+                                            </Button>
+                                        </Link>
+                                        <Link href={`/employee/${companyId}/me`}>
+                                            <Button variant={"ghost"} className="w-full flex flex-row gap-1 items-center justify-start has-[>svg]:p-2">
+                                                <UserCircleIcon />
+                                                <p className="text-sm font-medium">Profile Anda</p>
+                                            </Button>
+                                        </Link>
+                                        <Link href={`/employee/${companyId}/payroll/live`}>
+                                            <Button variant={"ghost"} className="w-full flex flex-row gap-1 items-center justify-start has-[>svg]:p-2">
+                                                <WalletIcon />
+                                                <p className="text-sm font-medium">Estimasi Gaji</p>
+                                            </Button>
+                                        </Link>
+                                        <Link href={`/employee/${companyId}/payroll/history`}>
+                                            <Button variant={"ghost"} className="w-full flex flex-row gap-1 items-center justify-start has-[>svg]:p-2">
+                                                <ReceiptText />
+                                                <p className="text-sm font-medium">Riwayat Gaji</p>
+                                            </Button>
+                                        </Link>
+                                        <Link href={`/employee/${companyId}/attendance`}>
+                                            <Button variant={"ghost"} className="w-full flex flex-row gap-1 items-center justify-start has-[>svg]:p-2">
+                                                <CalendarCheck2 />
+                                                <p className="text-sm font-medium">Absensi Mandiri</p>
+                                            </Button>
+                                        </Link>
+                                    </div>
+                                    <Separator />
+                                    <div className="w-full p-2">
+                                        <LogOutAlertDialog />
+                                    </div>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        </SidebarMenuItem>
+                    )}
+                </SidebarMenu>
+            </SidebarFooter>
+        </Sidebar>
+    )
+}
+
+function LogOutAlertDialog() {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const router = useRouter();
+
+    const employeeLogOut = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await logOutEmployee();
+
+            if (response.data.success === false) {
+                toast.custom(() => <Toaster variant="error" title="gagal melakukan logout" description={`${response.data.message || "kami gagal dalam memproses logout pada akun anda."}`} />);
+                return;
+            }
+
+            toast.custom(() => <Toaster variant="success" title="anda berhasil keluar dari akun" description="silahkan masuk kembali ke akun anda." />);
+            router.push("/signIn");
+        } catch (error) {
+            toast.custom(() => <Toaster variant="error" title="kami tidak bisa memproses" description={`${error || "terjadi suatu error sehingga kami tidak bisa memproses."}`} />)
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                {/* alert trigger */}
+                <Button className="w-full flex flex-row items-center bg-destructive hover:bg-red-700">
+                    <LogOut />
+                    <p className="text-sm">Keluar dari akun anda</p>
+                </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent size="sm">
+                <AlertDialogHeader>
+                    <AlertDialogMedia className="bg-destructive/10 text-destructive dark:bg-destructive/20 dark:text-destructive">
+                        <LogOut />
+                    </AlertDialogMedia>
+                    <AlertDialogTitle>Keluar dari akun anda?</AlertDialogTitle>
+                    <AlertDialogDescription className="font-medium">
+                        apakah anda yakin ingin keluar dari akun anda? ini akan membuat anda masuk ulang kedalam akun anda.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel variant="outline" className="w-full">
+                        <p className="text-sm font-medium">Cancel</p>
+                    </AlertDialogCancel>
+                    <AlertDialogAction variant="destructive" className="w-full" onClick={employeeLogOut}>
+                        {isLoading ? (<Spinner className="size-3.5 text-white" />) : (<p className="text-sm font-medium">Logout</p>)}
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    )
+}
